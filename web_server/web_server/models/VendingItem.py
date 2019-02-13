360921e73f134b896e42ab8c8d4f0ee48ce5ce4d
unknown <lsummerv@ucsd.edu>
2019-02-12 17:53:32 -0800

    Added fields to vending item

--- web_server/web_server/models/VendingItem.py
+++ web_server/web_server/models/VendingItem.py
@@ -1,8 +1,10 @@
+from django.db import models
+
 class VendingItem(models.Model):
     # TODO add fields
     #id: int
     #name: string
     #description: string
-    id = models.IntField(primary_key=True)
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=500)
